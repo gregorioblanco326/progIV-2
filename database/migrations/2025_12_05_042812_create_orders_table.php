5899adec100a43029e5f81499f3e1dc1821d160a
Gregorio <gregco@gmail.com>
2025-12-05 00:31:00 -0400

    .

--- database/migrations/2025_12_05_042812_create_orders_table.php
+++ database/migrations/2025_12_05_042812_create_orders_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
 
-            // Relación: Un pedido pertenece a un usuario
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
 
             $table->decimal('monto_total', 10, 2);
--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::resource('orders', OrdenController::class)->names([
     'index' => 'orders.listar',
-    'create' => 'orders.formularioCrear',
     'store' => 'orders.crear',
     'show' => 'orders.ver',
-    'edit' => 'orders.formularioEditar',
     'update' => 'orders.actualizar',
     'destroy' => 'orders.eliminar',
 ]);

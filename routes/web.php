bdcc4a155f98626d00e7d778a6c6481bf65a607d
Gregorio <gregco@gmail.com>
2025-12-05 00:17:57 -0400

    .

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::resource('profiles', PerfilController::class)->names([
     'index' => 'profiles.listar',
-    'create' => 'profiles.formularioCrear',
     'store' => 'profiles.crear',
     'show' => 'profiles.ver',
-    'edit' => 'profiles.formularioEditar',
     'update' => 'profiles.actualizar',
     'destroy' => 'profiles.eliminar',
 ]);
